b6e2d06dc44747efecffb1f681f598286a47a087
Liuzilong <goi82a@163.com>
2021-09-10 17:49:06 +0800

    add 3 func

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace GetPoterys
 {
     class Program
@@ -7,6 +7,24 @@ namespace GetPoterys
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+
+        public static int SumArray(List<int> list)
+        {
+            return 1;
+        }
+
+
+        public static void GetPotery()
+        {
+
+        }
+
+        
+        public static bool IsGetSuccesed()
+        {
+            return true;
         }
     }
 }
