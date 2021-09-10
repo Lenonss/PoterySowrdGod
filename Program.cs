937ee22cbd0dbf3f391709b5f098007e738ff570
Liuzilong <goi82a@163.com>
2021-09-10 17:54:05 +0800

    功能3修改

--- Program.cs
+++ Program.cs
@@ -24,6 +24,7 @@ namespace GetPoterys
         
         public static bool IsGetSuccesed()
         {
+            Console.WriteLine("Begin IsGetSuccesed");
             return true;
         }
     }
